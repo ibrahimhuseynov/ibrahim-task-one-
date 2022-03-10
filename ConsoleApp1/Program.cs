c8ccf0ecda33e231d17d1274a232685186874227
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-10 10:36:32 +0400

    aztu 20013

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 23, 34 };
+         foreach (int i in number(ref arr, 11))
+            {
+                Console.WriteLine(i);
+                Console.Read();
+            } 
+           
+        }
+        static int[] number(ref int[] arr,int num)
+        {
+            for (int i = 0; i <arr.Length; i++)
+            {
+                int s = arr[i] + num;
+                
+            }
+           return arr;
+        }
+    }
+}
